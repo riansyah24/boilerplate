e1f920a95ca98c62224b9a76a8217dda595a9770
riansyah24 <jackrian72@gmail.com>
2025-04-04 09:03:39 +0700

    feat: add lint

--- src/tests/Test.test.tsx
+++ src/tests/Test.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, beforeEach, expect } from "vitest"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Home from "../pages/Home.tsx"
 
 describe("Unit test", () => {
